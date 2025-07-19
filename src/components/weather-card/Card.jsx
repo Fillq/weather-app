3375bf4ff3b83d2bc8adf3e5efc967c16c8929e3
Fillq <filip007p@gmail.com>
2025-07-19 18:15:04 +0200

    minor changes to the Card.jsx file

--- src/components/weather-card/Card.jsx
+++ src/components/weather-card/Card.jsx
@@ -32,8 +32,8 @@ const Card = (props) => {
                         <div className="flex flex-row justify-center items-center p-3">  
                             <img src={ThermometerIcon} alt="temperature icon" title={weatherData.temp_c} className='h-8'/>
                             <div className="flex flex-col justify-center items-center p-3">  
-                                <h3 className='text-2xl font-bold'>{preferedUnit === 'C' ? weatherData.temp_c : weatherData.temp_f}&deg;{preferedUnit}</h3>
-                                <h3 className='text-xs font-light tracking-tight -mt-1'>(Feels like {preferedUnit === 'C' ? weatherData.feelslike_c : weatherData.feelslike_f}&deg;{preferedUnit})</h3>
+                                <h3 className='text-2xl font-bold'>{preferedUnit === 'F' ? weatherData.temp_f : weatherData.temp_c}&deg;{preferedUnit}</h3>
+                                <h3 className='text-xs font-light tracking-tight -mt-1'>(Feels like {preferedUnit === 'F' ? weatherData.feelslike_f : weatherData.feelslike_c}&deg;{preferedUnit})</h3>
                             </div>
                         </div>
                         <div className="flex flex-row justify-center items-center p-3 ">  
@@ -51,7 +51,7 @@ const Card = (props) => {
                             <div className="flex flex-row justify-center items-center p-3">
                                 <img src={day.day.condition.icon} alt="weather icon" title={day.day.condition.text} className='h-12'/>
                                 <div className="flex flex-col justify-center items-center p-3">  
-                                    <h3 className='text-2xl font-bold'>{preferedUnit === 'C' ? day.day.avgtemp_c : day.day.avgtemp_f}&deg;{preferedUnit}</h3>
+                                    <h3 className='text-2xl font-bold'>{preferedUnit === 'F' ? day.day.avgtemp_f : day.day.avgtemp_c}&deg;{preferedUnit}</h3>
                                     <h3 className='text-xs font-light tracking-tight -mt-1'>{day.day.condition.text}</h3>
                                 </div>
                             </div>
